ae1bfdb8b945f077e2cf779fade0c7a6bcb4d27c
Elzbieta Szymichowska <ela.buu@gmail.com>
2024-05-15 17:46:57 +0100

    ui for edit/new/show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -60,13 +60,14 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
